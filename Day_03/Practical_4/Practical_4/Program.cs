83dcf3de574ecb5a2d8d882ae80a6c5be0c5b563
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-24 17:23:50 +0400

    finish practical_4

---
+++ Day_03/Practical_4/Practical_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practical_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birth year: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+            int signNumber = birthYear % 12;
+
+            string animal = signNumber switch
+            {
+                0 => "Monkey",
+                1 => "Rooster",
+                2 => "Dog",
+                3 => "Boar / Pig",
+                4 => "Rat",
+                5 => "Ox",
+                6 => "Tiger",
+                7 => "Rabbit",
+                8 => "Dragon",
+                9 => "Snake",
+                10 => "Horse",
+                11 => "Sheep",
+                _ => throw new NotImplementedException(),
+            };
+
+            Console.WriteLine($"{birthYear} was {animal} year");
+        }
+    }
+}
