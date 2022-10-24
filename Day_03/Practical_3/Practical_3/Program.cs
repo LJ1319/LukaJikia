541cdc8eeb73992a42d56b26451c52aba2e5ac12
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-24 17:06:04 +0400

    finish practical_3

---
+++ Day_03/Practical_3/Practical_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practical_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double powValue = 2;
+
+            Console.WriteLine("Enter number: ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+
+            double result = Math.Pow(userInput, powValue);
+            Console.WriteLine($"The pow of the entered number is {result}");
+        }
+    }
+}
