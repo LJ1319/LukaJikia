27913ef551d0b55ef2c676aee8f4d94b83d47a91
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-24 16:46:48 +0400

    finish practical_1

---
+++ Day_03/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practical_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput % 2  == 0)
+            {
+                Console.WriteLine($"Entered number {userInput} is even");
+            } else
+            {
+                Console.WriteLine($"Entered number {userInput} is odd");
+            }
+        }
+    }
+}
