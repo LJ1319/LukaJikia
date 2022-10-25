50176b5d437ef2a421421299c5d605c7f38d8b6d
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-26 01:24:36 +0400

    finish practice_3

---
+++ Day_04/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int powValue = 3;
+
+            Console.WriteLine("Enter a number: ");
+            long userInput = Convert.ToInt64(Console.ReadLine());
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                long cubed = (long)Math.Pow(i, powValue);
+                Console.WriteLine($"{i} cubed is {cubed}");
+            }
+        }
+    }
+}
