5678d9914fccaf5c8cda34337b9ef6d0dd05342a
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-26 02:08:56 +0400

    finish practice_5

---
+++ Day_04/Practice_5/Practice_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practice_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number of rows of Floyd's trianlge to be printed: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if ((i % 2) == (j % 2))
+                    {
+                        Console.Write("1 ");
+
+                    } else
+                    {
+                        Console.Write("0 ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
