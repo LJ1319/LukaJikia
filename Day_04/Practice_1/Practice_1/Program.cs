595b9463caf48aeb2087ed15dd5491692c03f171
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-25 22:22:43 +0400

    finish practice_1

---
+++ Day_04/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Sum from 0 to 10 is 55");
+        }
+    }
+}
