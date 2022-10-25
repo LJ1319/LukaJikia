678ac6f1929f192f6390b60f3b4f7d55b638f430
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-26 01:38:34 +0400

    finish practice_4

---
+++ Day_04/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= userInput; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    //Console.WriteLine(i);
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"sum of odd numbers from 1 to {userInput} is {sum}");
+        }
+    }
+}
