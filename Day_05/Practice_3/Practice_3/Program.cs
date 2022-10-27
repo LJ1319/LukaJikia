2ea11333125807d10a15e660076a338ba980c2c0
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-28 01:16:11 +0400

    finish practice_3

---
+++ Day_05/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] userArray = new int[userInput];
+
+            int sum = 0;
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.Write($"Enter number for index {i}: ");
+                userArray[i] = Convert.ToInt32(Console.ReadLine());
+
+                sum += userArray[i];
+            }
+
+            Console.WriteLine($"Sum of array elements is {sum}");
+        }
+    }
+}
