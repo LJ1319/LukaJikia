c3a32290836b3a128b7a2d89fcb097fb66d47071
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-28 01:12:09 +0400

    finish practice_2

---
+++ Day_05/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] userArray = new int[userInput];
+
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.Write($"Enter number for index {i}: ");
+                userArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Here is your array in reverse order!");
+            for (int i = userInput - 1; i >= 0; i--)
+            {
+                Console.WriteLine(userArray[i]);
+            }
+        }
+    }
+}
