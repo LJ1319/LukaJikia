5370d7f12abba32824ef66da005f377d766d01e6
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-28 01:23:10 +0400

    finish practice_4

---
+++ Day_05/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] userArray = new int[userInput];
+
+            long product = 1;
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.Write($"Enter number for index {i}: ");
+                userArray[i] = Convert.ToInt32(Console.ReadLine());
+
+                product *= userArray[i];
+            }
+
+            Console.WriteLine($"Product of array elements is {product}");
+        }
+    }
+}
