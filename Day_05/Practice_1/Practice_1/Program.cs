0bfb931522cf06c665e4636b714b318e7f6af920
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-28 01:01:08 +0400

    finish practice_1

---
+++ Day_05/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            
+            int[] userArray = new int[userInput];
+
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.Write($"Enter number for index {i}: ");
+                userArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Here is your array!");
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.WriteLine(userArray[i]);
+            }
+        }
+    }
+}
