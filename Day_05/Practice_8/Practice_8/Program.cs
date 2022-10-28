b60f424769c477f13063dfe4874b744f32efce1a
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-28 17:31:11 +0400

    finish practice_8

---
+++ Day_05/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Practice_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 8;
+            int cols = 8;
+
+            Console.WriteLine("using multidimensional array");
+            int[,] numbers = new int[8, 8]
+            {
+                { 0, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+
+            };
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write($"Row {i + 1}: ");
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{numbers[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                Console.Write($"Row {i + 1}: ");
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{numbers[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("using jagged array");
+            int[][] jaggNums = new int[8][]
+            {
+                new int[] { 0, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 0, 0, 1, 1, 1, 1, 1, 1 },
+                new int[] { 0, 0, 0, 1, 1, 1, 1, 1 },
+                new int[] { 0, 0, 0, 0, 1, 1, 1, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1, 1, 1 },
+                new int[] { 0, 0, 0, 0, 0, 0, 1, 1 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write($"Row {i + 1}: ");
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{jaggNums[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                Console.Write($"Row {i + 1}: ");
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{jaggNums[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
