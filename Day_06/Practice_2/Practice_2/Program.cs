d797abe22a40287560b558efa7ddafd0ec0a9a73
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-31 15:13:14 +0400

    finish practice_2

---
+++ Day_06/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userArr = { 1, 3, 123, 15, 13, 23, 98 };
+            int index = 6;
+            SumOfDigits(userArr, index);
+        }
+
+        static int SumOfDigits(int[] userArr, int index)
+        {
+            int numAtIndx = userArr[index];
+
+            int sum = 0, rem;
+
+            while(numAtIndx > 0)
+            {
+                rem = numAtIndx % 10;
+                sum += rem;
+                numAtIndx /= 10;
+            }
+
+            Console.WriteLine($"The sum of the digits of a number at index {index} is {sum}");
+
+            return sum;
+        }
+    }
+}
