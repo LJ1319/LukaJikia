a3a3b378bec08ae6a623e1e3847b93511d0bec30
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-31 18:52:28 +0400

    finish practice_8

---
+++ Day_06/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DemonstrateNumber(GetNumber());
+        }
+
+        static int GetNumber()
+        {
+            Console.Write("Enter a positive number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Number must be positive");
+                return 1;
+            }
+
+            return number;
+        }
+
+        static void DemonstrateNumber(int number)
+        {
+            int count = 0;
+            Console.Write($"{number} = ");
+            while (number != 0)
+            {
+                count++;
+                number = (int)(number - number % Math.Pow(10, count));
+                Console.Write($"{count} * 10^{count - 1} + ");
+                //Console.WriteLine(number);
+            }
+
+        }
+    }
+}
