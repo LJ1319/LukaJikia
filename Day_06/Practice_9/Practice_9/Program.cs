997132dad4b647be512f623089c0b7c4061a9e74
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-10-31 18:57:44 +0400

    finish practice_9

---
+++ Day_06/Practice_9/Practice_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product(1, 3, 4, 15, 13, 23, 98);
+        }
+
+        static int Product(params int[] userArr)
+        {
+            int product = 1;
+
+            for (int i = 0; i < userArr.Length; i++)
+            {
+                product *= userArr[i];
+            }
+
+            Console.WriteLine($"The product pf array elements is: {product}");
+
+            return product; 
+        }
+    }
+}
