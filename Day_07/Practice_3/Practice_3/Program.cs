f89efdcfbdd28d6ce9bfdfdff71052901802357e
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-02 17:35:34 +0400

    finish practice_3

---
+++ Day_07/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an input string: ");
+            string input = Console.ReadLine();
+
+            SeperateWithSpaces(input);
+        }
+
+        static void SeperateWithSpaces(string input)
+        {
+            int length = input.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
