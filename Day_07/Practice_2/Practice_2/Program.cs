294b0423243406f87615d3b4f71182edf7d16b85
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-02 17:30:14 +0400

    finish practice_2

---
+++ Day_07/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an input string: ");
+            string input = Console.ReadLine();
+            Console.Write("Input string in reverse order: ");
+            PrintReverseString(input);
+        }
+
+        static void PrintReverseString(string input)
+        {
+            int length = input.Length;
+            // v1
+            //for (int i = 0; i < length; i++)
+            //{
+            //    Console.Write(input[length - i - 1]);
+            //}
+
+            // v2
+            for (int i = length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+        }
+    }
+}
