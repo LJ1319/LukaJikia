e5c42479383817f98b5af79358b429dd6d8447f4
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-07 16:58:47 +0400

    finish practice_2

---
+++ Day_08/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = SumOfIntegers(num);
+            Console.WriteLine(sum);
+        }
+
+        static int SumOfIntegers(int num)
+        {
+            if (num <= 1)
+                return 1;
+            else
+                return num + SumOfIntegers(num - 1);
+        }
+    }
+}
