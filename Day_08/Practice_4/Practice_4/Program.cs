07641d8be38ebe9792d72e53cc5f2bf37c79c42c
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-07 18:06:26 +0400

    finish practice_4

---
+++ Day_08/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter power: ");
+            int power = Convert.ToInt32(Console.ReadLine());
+
+            int result = PowerOfNumber(num, power);
+            Console.WriteLine(result);
+        }
+
+        static int PowerOfNumber(int num, int power)
+        {
+            if (power == 0)
+                return 1;
+            return num * PowerOfNumber(num, power - 1);
+        }
+    }
+}
