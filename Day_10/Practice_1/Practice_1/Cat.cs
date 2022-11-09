838df31f42738ab6ca16ee3484795f379d230fb5
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-09 16:26:41 +0400

    finish practice_1

---
+++ Day_10/Practice_1/Practice_1/Cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Practice_1
+{
+    internal class Cat
+    {
+        string _name;
+        string _breed;
+        int _age;
+
+        readonly int _oneBiteInGrams = 10;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value);
+        }
+
+        public string Breed
+        {
+            get => _breed;
+            set => _breed = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value);
+        }
+
+        public int Age
+        {
+            get { return _age; }
+
+            set
+            {
+                if (value > 0)
+                    _age = value;
+            }
+        }
+
+        public string Gender
+        { get; set; }
+
+        public void Meow(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Meowing...");
+            }
+        }
+
+        public void Eat(int foodWeightInGrams)
+        {
+            Console.WriteLine($"{_name} start eating.");
+            //int bites = 0;
+
+            while (foodWeightInGrams > 0)
+            {
+                foodWeightInGrams -= _oneBiteInGrams;
+                //bites++;
+                Console.WriteLine("Eating...");
+            }
+
+            Console.WriteLine($"{_name} finished eating.");
+        }
+    }
+}
---
+++ Day_10/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Transactions;
+
+namespace Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating Cat object...");
+            Cat cat1 = new Cat();
+
+            Console.Write("Enter name: ");
+            cat1.Name = Console.ReadLine();
+
+            Console.Write("Enter breed: ");
+            cat1.Breed = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            cat1.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter sex: ");
+            cat1.Gender = Console.ReadLine();
+
+            Console.WriteLine("Cat object created.");
+
+            Console.Write("Enter food weight in grams: ");
+            int foodWeightInGrams = Convert.ToInt32(Console.ReadLine());
+            cat1.Eat(foodWeightInGrams);
+
+            Console.Write("Enter meowing count: ");
+            int meowingCount = Convert.ToInt32(Console.ReadLine());
+            cat1.Meow(meowingCount);
+        }
+    }
+}
