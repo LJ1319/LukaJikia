b9616cd14c8b299ca7ac60bdae06cd5f1513563b
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-09 18:05:35 +0400

    finish practice_2

---
+++ Day_10/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            int firstSide = Convert.ToInt32(Console.ReadLine());
+
+            if (firstSide <= 0)
+            {
+                Console.WriteLine("Side must be greater than zero.");
+                return;
+            }
+
+            Console.Write("Enter side 2: ");
+            int secondSide = Convert.ToInt32(Console.ReadLine());
+
+            if (secondSide <= 0)
+            {
+                Console.WriteLine("Side must be greater than zero.");
+                return;
+            }
+
+            Console.Write("Enter side 3: ");
+            int thirdSide = Convert.ToInt32(Console.ReadLine());
+
+            if (thirdSide <= 0)
+            {
+                Console.WriteLine("Side must be greater than zero.");
+                return;
+            }
+
+            Triangle triangle1 = new Triangle();
+            triangle1.FirtSide = firstSide;
+            triangle1.SecondSide = secondSide;
+            triangle1.ThirdSide = thirdSide;
+
+            if (IsValidTriangle(triangle1))
+            {
+                int perimeter = triangle1.Perimeter();
+                Console.WriteLine($"Perimeter of the triangle is: {perimeter}");
+
+                int area = triangle1.Area();
+                Console.WriteLine($"Area of the triangle is: {area}");
+            }
+        }
+
+        static bool IsValidTriangle(Triangle triangle)
+        {
+            if (triangle.FirtSide + triangle.SecondSide > triangle.ThirdSide &&
+                triangle.FirtSide + triangle.ThirdSide > triangle.SecondSide &&
+                triangle.SecondSide + triangle.ThirdSide > triangle.FirtSide
+                )
+            {
+                triangle.IsValidTriangle = true;
+                return true;
+            }
+
+            Console.WriteLine("It is not a valid triangle");
+
+            return false;
+        }
+    }
+}
---
+++ Day_10/Practice_2/Practice_2/Triangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_2
+{
+    internal class Triangle
+    {
+        int _firstSide;
+        int _secondSide;
+        int _thirdSide;
+
+        public int FirtSide
+        {
+            get { return _firstSide; }
+            set
+            {
+                if (value > 0)
+                    _firstSide = value;
+            }
+        }
+
+        public int SecondSide
+        {
+            get { return _secondSide; }
+            set
+            {
+                if (value > 0)
+                    _secondSide = value;
+            }
+        }
+
+        public int ThirdSide
+        {
+            get { return _thirdSide; }
+            set
+            {
+                if (value > 0)
+                    _thirdSide = value;
+            }
+        }
+
+        public bool IsValidTriangle
+        { get; set; }
+
+
+        public int Perimeter()
+        {
+            if (IsValidTriangle)
+            {
+                return _firstSide + _secondSide + _thirdSide;
+            }
+
+            return 0;
+        }
+
+        public int Area()
+        {
+            if (IsValidTriangle)
+            {
+                return Perimeter() / 2;
+            }
+
+            return 0;
+        }
+    }
+}
