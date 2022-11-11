a8c2dbc137f30716673c877cfcc2f567a10bf02c
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-11 17:04:44 +0400

    finish practice_1

---
+++ Day_11/Practice_1/Practice_1/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string PersonalIdNumber { get; set; }
+
+        public Home Home { get; set; }
+    }
+}
