96a48d2c69ecb59fa83838aadbb384f3ea72d562
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-14 17:28:06 +0400

    finish practices

---
+++ Day_12/Practices/Practices/MATH.cs
@@ -0,0 +1,67 @@
+﻿namespace Practices
+{
+    internal static class MATH
+    {
+        public static double POW(double number, double power, out STATUSES.Statuses status)
+        {
+            double result = 1;
+
+            if (power < 0)
+            {
+                status = STATUSES.Statuses.PowMustBePositiveOrZero;
+                return -1;
+            }
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            status = STATUSES.Statuses.Success;
+
+            return result;
+        }
+
+        public static double MIN(double firstNum, double secondNum, out STATUSES.Statuses status)
+        {
+            double min;
+            if (firstNum < secondNum)
+            {
+                min = firstNum;
+            }
+            else if (secondNum < firstNum)
+            {
+                min = secondNum;
+            }
+            else
+            {
+                status = STATUSES.Statuses.IdenticalParameters;
+                return -1;
+            }
+
+            status = STATUSES.Statuses.DifferentParameters;
+            return min;
+        }
+
+        public static double MAX(double firstNum, double secondNum, out STATUSES.Statuses status)
+        {
+            double max;
+
+            if (firstNum > secondNum)
+            {
+                max = firstNum;
+            }
+            else if (secondNum > firstNum)
+            {
+                max = secondNum;
+            }
+            else
+            {
+                status = STATUSES.Statuses.IdenticalParameters;
+                return -1;
+            }
+
+            status = STATUSES.Statuses.DifferentParameters;
+            return max;
+        }
+    }
+}
---
+++ Day_12/Practices/Practices/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Practices
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a power: ");
+            double power = Convert.ToDouble(Console.ReadLine());
+
+            double result = MATH.POW(number, power, out STATUSES.Statuses powStatus);
+
+            Console.WriteLine(powStatus);
+            if (powStatus == STATUSES.Statuses.Success)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("-------------------------");
+
+            Console.Write("Enter first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            STATUSES.Statuses minMaxStatus;
+            double min = MATH.MIN(firstNumber, secondNumber, out minMaxStatus);
+
+            double max = MATH.MAX(firstNumber, secondNumber, out minMaxStatus);
+
+            Console.WriteLine(minMaxStatus);
+            if (minMaxStatus == STATUSES.Statuses.DifferentParameters)
+            {
+                Console.WriteLine($"Min value is {min}");
+                Console.WriteLine($"Max value is {max}");
+            }
+        }
+    }
+}
