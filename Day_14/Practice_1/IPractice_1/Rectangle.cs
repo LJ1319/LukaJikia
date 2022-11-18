0f335e980ba6010957d25702c739200c3820d07b
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-18 18:44:50 +0400

    finish practice_1 using interface

---
+++ Day_14/Practice_1/IPractice_1/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿namespace Practice_1
+{
+    class Rectangle : IShape
+    {
+        public Point A { get; set; } = new Point();
+        public Point B { get; set; } = new Point();
+        public Point C { get; set; } = new Point();
+        public Point D { get; set; } = new Point();
+
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public double SideD { get; set; }
+
+        private double _perimeter;
+        private double _area;
+
+        public Rectangle(int aX, int aY, int bX, int bY, int cX, int cY, int dX, int dY)
+        {
+            A.X = aX;
+            A.Y = aY;
+            B.X = bX;
+            B.Y = bY;
+            C.X = cX;
+            C.Y = cY;
+            D.X = dX;
+            D.Y = dY;
+
+            SideA = Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+            SideB = Math.Sqrt(Math.Pow(B.X - C.X, 2) + Math.Pow(B.Y - C.Y, 2));
+            SideC = Math.Sqrt(Math.Pow(A.X - C.X, 2) + Math.Pow(A.Y - C.Y, 2));
+            SideD = Math.Sqrt(Math.Pow(A.X - C.X, 2) + Math.Pow(A.Y - C.Y, 2));
+        }
+
+        public double Perimeter()
+        {
+            _perimeter = SideA + SideB + SideC + SideD;
+            return _perimeter;
+        }
+
+        public double Area()
+        {
+            _area = SideA * SideB;
+            return _area;
+        }
+    }
+}
---
+++ Day_14/Practice_1/IPractice_1/Triangle.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice_1
+{
+    public class Triangle : IShape
+    {
+        public Point A { get; set; } = new Point();
+        public Point B { get; set; } = new Point();
+        public Point C { get; set; } = new Point();
+
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        private double _perimeter;
+        private double _area;
+
+        public Triangle(int aX, int aY, int bX, int bY, int cX, int cY)
+        {
+            A.X = aX;
+            A.Y = aY;
+            B.X = bX;
+            B.Y = bY;
+            C.X = cX;
+            C.Y = cY;
+
+            SideA = Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+            SideB = Math.Sqrt(Math.Pow(B.X - C.X, 2) + Math.Pow(B.Y - C.Y, 2));
+            SideC = Math.Sqrt(Math.Pow(A.X - C.X, 2) + Math.Pow(A.Y - C.Y, 2));
+        }
+
+        public double Perimeter()
+        {
+            _perimeter = SideA + SideB + SideC;
+            return _perimeter;
+        }
+
+        public double Area()
+        {
+            _area = _perimeter / 2;
+            return _area;
+        }
+    }
+}
