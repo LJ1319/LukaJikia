e6484b8922bb3b7ecc484a67f59200256c2796f7
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-23 18:55:05 +0400

    finish practice_1

---
+++ Day_16/Practice_1/Practice_1/Matrix.cs
@@ -0,0 +1,72 @@
+﻿namespace Practice_1
+{
+    public class Matrix
+    {
+        private int[,] matrix = new int[2, 2];
+
+        public Matrix(int rows, int cols)
+        {
+            Rows = rows;
+            Columns = cols;
+        }
+
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public int this[int rowIndex, int colIndex]
+        {
+            get => matrix[rowIndex, colIndex];
+            set => matrix[rowIndex, colIndex] = value;
+        }
+
+        public static int[,] operator +(Matrix matrix1, Matrix matrix2)
+        {
+            int[,] resultMatrix = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    resultMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static int[,] operator -(Matrix matrix1, Matrix matrix2)
+        {
+            int[,] resultMatrix = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    resultMatrix[i, j] = matrix1[i, j] - matrix2[i, j];
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static int[,] operator *(Matrix matrix1, Matrix matrix2)
+        {
+            int[,] resultMatrix = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static implicit operator Matrix(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day_16/Practice_1/Practice_1/VectorMathOperation.cs
@@ -0,0 +1,72 @@
+﻿namespace Practice_1
+{
+    public class VectorMathOperation : IMathOperation<Vector>
+    {
+        public Vector[] Vectors { get; set; }
+
+        public int Scalar { get; set; } 
+
+        public VectorMathOperation(params Vector[] vectors)
+        {
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                vectors[i] = new Vector();
+            }
+            Vectors = vectors;
+        }
+
+        public VectorMathOperation(int scalar, params Vector[] vectors)
+        {
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                vectors[i] = new Vector();
+            }
+
+            Vectors = vectors;
+            Scalar = scalar;
+        }
+
+        public Vector Addition(params Vector[] vectors)
+        {
+            Vector addVect = new Vector();
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                for (int j = 0; j < vectors.Length; j++)
+                {
+                    addVect = new Vector(Vectors[i].X + Vectors[j].X, Vectors[i].Y + Vectors[j].Y);
+                }
+                
+            }
+
+            return addVect;
+        }
+
+
+        public Vector Substraction(params Vector[] vectors)
+        {
+            Vector addVect = new Vector();
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                for (int j = 0; j < vectors.Length; j++)
+                {
+                    addVect = new Vector(Vectors[i].X - Vectors[j].X, Vectors[i].Y - Vectors[j].Y);
+                }
+
+            }
+
+            return addVect;
+        }
+
+        public Vector Multiplication(params Vector[] vectors)
+        {
+            Vector addVect = new Vector();
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                addVect = new Vector(Vectors[i].X * Scalar, Vectors[i].Y * Scalar);
+            }
+
+            return addVect;
+        }
+    }
+}
