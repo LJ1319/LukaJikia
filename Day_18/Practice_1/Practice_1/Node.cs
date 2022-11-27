7341b47eb6ce5a507985ab1c1039d98f776f8bfe
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-11-28 03:31:35 +0400

    finish practice_1

---
+++ Day_18/Practice_1/Practice_1/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice_1
+{
+    internal class Node
+    {
+        public int Data { get; set; }
+
+        public Node? Next { get; set; }
+    }
+}
---
+++ Day_18/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Practice_1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Node nodeA = new Node();
+            nodeA.Data = 13;
+
+            Node nodeB = new Node();
+            nodeB.Data = 17;
+
+            Node nodeC = new Node();
+            nodeC.Data = 19;
+
+            nodeA.Next = nodeB;
+            nodeB.Next = nodeC;
+
+            LinkedItem linkedItems = new LinkedItem();
+            linkedItems[0] = nodeA;
+            linkedItems[1] = nodeB;
+            linkedItems[2] = nodeC;
+
+            Node newNode = new Node()
+            {
+                Data = 15,
+            };
+
+            Console.Write("Enter an index: ");
+            int reqIndex = Convert.ToInt32(Console.ReadLine()); 
+
+            try
+            {
+                if (linkedItems[reqIndex] != newNode)
+                {
+                    throw new NotAListMemberException($"Requested element at given n-th place (index: {reqIndex}) was not found in the list!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
