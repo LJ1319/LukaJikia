567c59fa9fab2fde812768ddd666b20a15bc36d6
LJ1319 <luka.jikia.2@btu.edu.ge>
2022-12-09 18:41:35 +0400

    finish TestingTime

---
+++ Day_23/Practice/TestingTime/TestingTime.cs
@@ -0,0 +1,35 @@
+﻿namespace TestingTime
+{
+    internal class TestingTime
+    {
+        public List<string[]> text = new List<string[]>();
+
+        public List<string> correctAnswers = new List<string>();
+
+        public void startTest(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                text.Add(line.Split('|', '\n'));
+            }
+
+            foreach (var line in text)
+            {
+                foreach (var word in line)
+                {
+                    if (word.Contains('*'))
+                    {
+                        correctAnswers.Add(word.Replace('*', ' '));
+                    }
+                }
+            }
+        }
+
+        public void addTest(string filePath, List<string> userContent)
+        {
+            File.AppendAllLines(filePath, userContent);
+        }
+    }
+}
