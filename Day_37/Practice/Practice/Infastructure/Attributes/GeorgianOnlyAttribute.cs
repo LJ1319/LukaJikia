e45d917e849c309822e43d88b8fe0a32e6e9723f
LJ1319 <luka.jikia.2@btu.edu.ge>
2023-01-18 17:32:34 +0400

    update regex pattern properly

--- Day_37/Practice/Practice/Infastructure/Attributes/GeorgianOnlyAttribute.cs
+++ Day_37/Practice/Practice/Infastructure/Attributes/GeorgianOnlyAttribute.cs
@@ -5,7 +5,7 @@ namespace Practice.Infastructure.Attributes
 {
     public class GeorgianOnlyAttribute : ValidationAttribute
     {
-        private readonly string? pattern = @"[ა-ჰ]";
+        private readonly string? pattern = @"^[ა-ჰ]+$";
 
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
